3f22c386e5753b538dc98954337d188da9a6d1fc
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-03 16:34:12 +0000

    Specified key was too long - FIXED

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,17 @@
 <?php
+# @Author: John Carlo M. Ramos
+# @Date:   2019-11-03T16:29:43+00:00
+# @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-03T16:33:19+00:00
+
+
+
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      Schema::defaultStringLength(191);
     }
 }
