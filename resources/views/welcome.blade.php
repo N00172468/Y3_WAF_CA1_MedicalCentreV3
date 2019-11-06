e0bb86c01609fbd4935961850ba58a2873c68ba2
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-06 10:56:50 +0000

    Edited styling for Default Laravel Home View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Medical Centre</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -13,9 +13,10 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+                /* color: #0062ff; */
+                color: #7851a9;
+                font-family: 'Roboto', sans-serif;
+                font-weight: 300;
                 height: 100vh;
                 margin: 0;
             }
@@ -49,10 +50,11 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #b8860b;
                 padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
+                font-family: 'Raleway', sans-serif;
+                font-size: 16px;
+                font-weight: 500;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
@@ -60,6 +62,13 @@
 
             .m-b-md {
                 margin-bottom: 30px;
+            }
+
+            .divider {
+              height: 2px;
+              /* background-color: #b8860b; */
+              background-color: #7851a9;
+              margin-top: 1px;
             }
         </style>
     </head>
@@ -81,18 +90,19 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Eastwood Medical Centre
+                    <hr class="divider">
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://github.com/N00172468">Github</a>
+                    <a href="https://www.linkedin.com/in/john-carlo-ramos-1a587b195/">LinkedIn</a>
+                    <a href="https://www.instagram.com/jc_ramos_photography/">Instagram</a>
+                    {{-- <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
                 </div>
             </div>
         </div>
