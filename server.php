5e991789fcbf0530bd299f6c8be86008e2df5639
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-06 09:54:07 +0000

    Server.php FIXED

---
+++ server.php
@@ -0,0 +1,29 @@
+<?php
+# @Author: John Carlo M. Ramos
+# @Date:   2019-11-05T09:18:21+00:00
+# @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-05T14:30:30+00:00
+
+
+
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
