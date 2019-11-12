3e76ddff816e761a1e9f97ab840ec7217ac519e5
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-12 12:22:11 +0000

    CRUD for Admin-to-Visit

--- app/Visits.php
+++ app/Visits.php
@@ -1,4 +1,12 @@
 <?php
+# @Author: John Carlo M. Ramos
+# @Date:   2019-11-11T18:02:08+00:00
+# @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-12T12:04:51+00:00
+
+
+
 
 namespace App;
 
@@ -6,5 +14,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visits extends Model
 {
-    //
+    // protected $dates = ['name_field'];
 }
