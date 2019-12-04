5ced293a0742e5a80b44ff55dae5b93cf82f9896
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-12-04 10:07:22 +0000

    Created patient-index Page for Doctor Account

---
+++ resources/views/doctor/patients/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            Patients:
+            <a href="{{ route('doctor.patients.create') }}" class="btn btn-primary float-right">Add</a>
+          </div>
+          <div class="card-body">
+            @if (count($patients) === 0)
+              <p>There are no Patients on record!</p>
+            @else
+              <table id="table-patients" class="table table-hover">
+                <thead>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                  <th>Health Insurance</th>
+                  <th>Policy Number</th>
+                  <th>Patient I.D</th>
+                </thead>
+                <tbody>
+                  @foreach ($patients as $patient)
+                    <tr data-id="{{ $patient->id }}">
+                      <td>{{ $patient->user->name }}</td>
+                      <td>{{ $patient->user->address }}</td>
+                      <td>{{ $patient->user->phone }}</td>
+                      <td>{{ $patient->user->email }}</td>
+                      <td>{{ $patient->health_insurance }}</td>
+                      <td>{{ $patient->policy_no }}</td>
+                      <td>{{ $patient->id }}</td>
+                      <td>
+                        <a href="{{ route('doctor.patients.show', $patient->id) }}" class="btn btn-info">View</a>
+                        <a href="{{ route('doctor.patients.edit', $patient->id) }}" class="btn btn-warning">Edit</a>
+                        <form style="display:inline-block" method="POST" action="{{ route('doctor.patients.destroy', $patient->id) }}">
+                          <input type="hidden" name="_method" value="DELETE">
+                          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                          <button type="submit" class="form-control btn btn-danger">Delete</a>
+                        </form>
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
