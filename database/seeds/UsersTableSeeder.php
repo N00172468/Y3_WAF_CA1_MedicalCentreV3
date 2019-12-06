d42a5804416337960465495a6b22721af5426b1d
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-12-06 10:25:25 +0000

    Updated User Seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -32,30 +32,102 @@ class UsersTableSeeder extends Seeder
 
       // Doctor User:
       $doctor = new User();
-      $doctor->name = "Dr. Hibbert";
-      $doctor->address = "3000, New New York, Delivery Express";
+      $doctor->name = "Julius M. Hibbert";
+      $doctor->address = "734 Evergreen Terrace, Springfield";
       $doctor->phone = "+123 456 7890";
+      $doctor->email = "doctor@email.com";
+      $doctor->password = bcrypt('secret');
+      $doctor->save();
+      $doctor->roles()->attach($role_doctor);
+
+      $doctor = new User();
+      $doctor->name = "Nick Riviera";
+      $doctor->address = "Somewhere in The Simpsons World";
+      $doctor->phone = "+312 416 5729";
+      $doctor->email = "hello.everybody@email.com";
+      $doctor->password = bcrypt('secret');
+      $doctor->save();
+      $doctor->roles()->attach($role_doctor);
+
+      $doctor = new User();
+      $doctor->name = "Meredith Grey";
+      $doctor->address = "Grey's Anatomy, MD";
+      $doctor->phone = "+047 628 3291";
+      $doctor->email = "grey.md@email.com";
+      $doctor->password = bcrypt('secret');
+      $doctor->save();
+      $doctor->roles()->attach($role_doctor);
+
+      $doctor = new User();
+      $doctor->name = "John A. Zoidberg";
+      $doctor->address = "3000, New New York, Delivery Express";
+      $doctor->phone = "+726 086 3000";
       $doctor->email = "futurama@email.com";
+      $doctor->password = bcrypt('secret');
+      $doctor->save();
+      $doctor->roles()->attach($role_doctor);
+
+      $doctor = new User();
+      $doctor->name = "Gregory House";
+      $doctor->address = "Murica";
+      $doctor->phone = "+383 829 1688";
+      $doctor->email = "house.md@email.com";
+      $doctor->password = bcrypt('secret');
+      $doctor->save();
+      $doctor->roles()->attach($role_doctor);
+
+      $doctor = new User();
+      $doctor->name = "Ken Jeong";
+      $doctor->address = "Icebox, Las Vegas Hotel ";
+      $doctor->phone = "+635 134 9271";
+      $doctor->email = "but.did.you.die@email.com";
       $doctor->password = bcrypt('secret');
       $doctor->save();
       $doctor->roles()->attach($role_doctor);
 
       // Patient User:
       $patient = new User();
-      $patient->name = "Mai Lehg";
-      $patient->address = "123 Fake St, Faking It";
+      $patient->name = "John Carlo M. Ramos";
+      $patient->address = "123 Lol Help, Pls";
       $patient->phone = "+098 765 4321";
-      $patient->email = "m.l@email.com";
+      $patient->email = "patient@email.com";
+      $patient->password = bcrypt('secret');
+      $patient->save();
+      $patient->roles()->attach($role_patient);
+
+      $patient = new User();
+      $patient->name = "Mai Legg";
+      $patient->address = "123 Faking It";
+      $patient->phone = "+539 029 1739";
+      $patient->email = "football.player@email.com";
+      $patient->password = bcrypt('secret');
+      $patient->save();
+      $patient->roles()->attach($role_patient);
+
+      $patient = new User();
+      $patient->name = "Guy Buddy";
+      $patient->address = "45, Fake Dr.";
+      $patient->phone = "+927 173 1739";
+      $patient->email = "buddy.guy@email.com";
+      $patient->password = bcrypt('secret');
+      $patient->save();
+      $patient->roles()->attach($role_patient);
+
+      $patient = new User();
+      $patient->name = "Stud Ent";
+      $patient->address = "92, Co. Carlow";
+      $patient->phone = "+917 912 1649";
+      $patient->email = "stud.ent@email.com";
       $patient->password = bcrypt('secret');
       $patient->save();
       $patient->roles()->attach($role_patient);
 
       // Admin User:
       $admin = new User();
-      $admin->name = "John Carlo M. Ramos";
-      $admin->address = "168 Gleann na Ri, Druid Valley, Cabinteely, Dublin 18";
-      $admin->phone = "+123 123 1234";
-      $admin->email = "j.c@email.com";
+      $admin->name = "Mo Cher";
+      $admin->address = "IADT, Kill Ave, Dun Laoghaire, Dublin";
+      $admin->phone = "+839 729 9273";
+      $admin->email = "admin@email.com";
       $admin->password = bcrypt('secret');
       $admin->save();
       $admin->roles()->attach($role_admin);
