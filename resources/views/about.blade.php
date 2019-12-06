765e211ca57404242f1a2e5612f836ae2cc5a819
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-12-06 13:40:26 +0000

    Updated About Page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,88 @@
+<style>
+  .title {
+    color: #b8860b;
+  }
+
+  .divider {
+    height: 2px;
+    background-color: #b8860b;
+    /* background-color: #7851a9; */
+    border: none;
+    margin-top: 1px;
+  }
+
+  .content {
+    color: #7a7a7a;
+  }
+</style>
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+  <div class="row justify-content-center">
+      <h1 class="title">
+        About
+      </h1>
+  </div>
+
+  <hr class="divider">
+
+  <div class="row justify-content-center text-center">
+      <h1 class="content display-1">
+        John Carlo M. Ramos
+      </h1>
+  </div>
+
+  <div class="row justify-content-center text-center">
+      <h1 class="content display-1">
+        DL836 | Year 3
+      </h1>
+  </div>
+
+  <div class="row justify-content-center text-center">
+      <h1 class="content display-1">
+        W.A.F
+      </h1>
+  </div>
+
+  <div class="row justify-content-center text-center">
+      <h1 class="content display-1">
+        CA1
+      </h1>
+  </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <div class="row justify-content-center text-center">
+      <h1 class="content display-1">
+        Fueled by coffee, <br/> living for coffee.
+      </h1>
+  </div>
+  <div class="row justify-content-center">
+      <p class="content">
+        Please send help 🙃
+      </p>
+  </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">About</div>
@@ -12,6 +92,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
